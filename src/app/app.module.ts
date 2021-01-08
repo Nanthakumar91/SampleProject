c3eea4dfc20e600a3fb077c7545c2e82e27a4922
Nanthakumar <snanthakumar91@outlook.com>
2021-01-08 21:23:02 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DisplayComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule
+ 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
